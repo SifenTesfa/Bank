9e6ae9b1e8315a54c86565ea2e73192b6b29884e
SifenTesfa <sifentt@gmail.com>
2024-08-20 11:48:26 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,12 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BanklistController;
+use App\Http\Controllers\BlogController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware(Authenticate::using('sanctum'));
 Route::post ("addBank",[BanklistController::class,"addBank"]);
 Route::get("list",[BanklistController::class,"list"]);
+Route::post ("addBlog",[BlogController::class,"addBlog"]);
+Route::get("listBlog",[BlogController::class,"listBlog"]);
